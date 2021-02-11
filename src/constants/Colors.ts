c98369ebe2c713c36a0f40013299da77c6ae2edd
devpoi <smboy86@naver.com>
2021-02-12 04:11:56 +0900

    update - constants

---
+++ src/constants/Colors.ts
@@ -0,0 +1,21 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    tint: tintColorLight,
+    text: '#000',
+    unChecked: '#CACACA',
+    background: '#fff',
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    tint: tintColorDark,
+    text: '#fff',
+    unChecked: '#CACACA',
+    background: '#000',
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
